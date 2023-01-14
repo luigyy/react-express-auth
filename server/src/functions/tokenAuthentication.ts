e08c7979363a3db475f37884543b116e7ca174df
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-13 20:53:46 -0600

    added function to verify token

--- server/src/functions/tokenAuthentication.ts
+++ server/src/functions/tokenAuthentication.ts
@@ -5,6 +5,12 @@ export function decodeToken(token: string) {
   const data = jwt.decode(token);
   return data;
 }
+
+export function veriyToken(token: string) {
+  const payload = jwt.verify(token, secret);
+  return payload;
+}
+
 //
 export function signToken(id: string) {
   const token = jwt.sign(
