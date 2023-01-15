4f1d440e10040e8ec713c7f58fac1c7e4b1013e3
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-14 18:53:05 -0600

    added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>hello </div>;
+  return <div className="text-center"> test </div>;
 }
 
 export default App;
