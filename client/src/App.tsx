88d4cd8fb71ff1fd69f92bb5d36ea0435a508761
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-17 19:18:35 -0600

    added buttons :

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 function App() {
-  return <div className="text-center"> test </div>;
+  return (
+    <div className="text-center  flex justify-center h-screen">
+      <div className=" flex gap-8 my-auto h-auto ">
+        <button className="bg-gray-200 px-3 py-2 rounded-3xl">
+          public route
+        </button>
+        <button className="bg-gray-200 px-3 py-2 rounded-3xl">Login</button>
+        <button className="bg-gray-200 px-3 py-2 rounded-3xl">
+          private route
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
